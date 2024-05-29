d9157a2a8b4a8d85816dfcad7d30ed93d37a99ff
Vladislav Orlov <orlov.v@livedigital.space>
2024-05-29 11:23:42 +0300

    fix: LD-6410 add random value for `uid`

--- web-sdk-ts/src/config.ts
+++ web-sdk-ts/src/config.ts
@@ -4,7 +4,7 @@ import jwt from 'jsonwebtoken';
 const jwtSecret = 'secret_of_your_client';
 const appId = 'your_app_id'
 const channelId = 'your_channel_id';
-const uid = '65fd7046789d2de7d98f2cd6'; // unique objectId, for example
+const uid = crypto.randomUUID().replace(/-/g, '').slice(0, 24); // unique objectId, for example
 const role = 'host' as Role;
 
 const token = jwt.sign({ channelId }, jwtSecret, { issuer: appId, subject: uid, audience: role });
